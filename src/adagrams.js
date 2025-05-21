5c43720b1b4fe9de605c42bc0b00bb9de22a5e18
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 17:27:11 -0400

    completed Wave1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,72 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterQuantities = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+};
+
+  const letterPool = [];
+
+  for (const [letter, qty] of Object.entries(letterQuantities)) {
+      // console.log(`${letter} appears ${qty} times`);
+      // Array(4) creates an array with 4 empty slots.
+      // .fill('A') fills all of them with 'A' → ['A', 'A', 'A', 'A']
+      // ... (spread operator) unpacks the array so that .push() adds each element individually.
+    letterPool.push(...Array(qty).fill(letter));
+  };
+
+  // console.log(letterPool)
+
+  const letterHand = [] // 10 letters
+
+  // create a random number between 0 to letterPool length - 1 inclusive
+  // check if the value of letterPool[rand] in letter quantities, 
+      // if quantities > 1
+          // add the letter to letterHand
+          // decrement the value in letterQuantities
+  const handSize = 10;
+
+  while (letterHand.length < handSize) {
+      // Math.random() → generates a number between 0 (inclusive) and 1 (exclusive).
+      // * letterPool.length → scales it to the size of the array.
+      // Math.floor() → rounds down to the nearest whole number, giving you an index from 0 to letterPool.length - 1.
+      // if letterPool.length is 5, this will give 0 - 4
+      const randomNum = Math.floor(Math.random() * letterPool.length);
+      // console.log(randomNum)
+      
+      const randletter = letterPool[randomNum]
+
+      if (letterQuantities[randletter] > 1) {
+          letterHand.push(randletter);
+          console.log(letterHand);
+          letterQuantities[randletter] -= 1
+      }
+  };
+  return letterHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
